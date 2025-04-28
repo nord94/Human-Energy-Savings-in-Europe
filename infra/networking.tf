6ebd774e2b09ba62535f3a71fb17173757d1cc68
Vasiliy Kiryushin <vasiliy.kiryushin@effem.com>
2025-04-28 15:03:11 +0200

    Edit networking

--- infra/networking.tf
+++ infra/networking.tf
@@ -1,6 +1,9 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
+  enable_dns_support   = true     # <-- Add this
+  enable_dns_hostnames = true     # <-- Add this
+
   tags = {
     Name = "airflow-vpc"
   }
